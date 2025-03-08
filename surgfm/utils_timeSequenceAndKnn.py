63fd0bd5a6d2acfaf8dabb37375b7e105f3a920c
anonymous-8989 <202156370+anonymous-8989@users.noreply.github.com>
2025-03-08 14:20:41 +0800

    Update utils_timeSequenceAndKnn.py

--- surgfm/utils_timeSequenceAndKnn.py
+++ surgfm/utils_timeSequenceAndKnn.py
@@ -649,8 +649,6 @@ def accuracy(output, target, topk=(1,)):
 
 
 def _no_grad_trunc_normal_(tensor, mean, std, a, b):
-    # Cut & paste from PyTorch official master until it's in a few official releases - RW
-    # Method based on https://people.sc.fsu.edu/~jburkardt/presentations/truncated_normal.pdf
     def norm_cdf(x):
         # Computes standard normal cumulative distribution function
         return (1. + math.erf(x / math.sqrt(2.))) / 2.
